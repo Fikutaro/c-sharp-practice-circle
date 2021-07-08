bc442318ee2182d8fb3a93a9d0c079de572ffb56
Fikutaro <nomio9029@gmail.com>
2021-07-08 17:05:20 +0900

    プロジェクト ファイルを追加します。

---
+++ Project62/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MinMax c = new MinMax();
+            //  円の半径を設定
+            c.r = 4.0;
+            Console.WriteLine("半径" + c.r + "の円の円周の長さは" + c.Circumference());
+            Console.WriteLine("半径" + c.r + "の円の面積は" + c.Area());
+        }
+    }
+}
